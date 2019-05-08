512368e74e929038fa156b52a9e5379032d95d69
Dariusz Okonski <darek200180@gmail.com>
2019-05-08 19:14:25 +0100

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  state = {}
+  render() {
+    return (
+      <div className='app'>
+        <h1>divination app</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
