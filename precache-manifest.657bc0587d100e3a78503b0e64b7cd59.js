2b8435a783b86f88f1feb81f297b34b3ab879c53
Dariusz Okonski <darek200180@gmail.com>
2019-05-17 18:45:04 +0100

    Updates

---
+++ precache-manifest.657bc0587d100e3a78503b0e64b7cd59.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "189980fcd114d6e29e86e569844f9571",
+    "url": "/react-simple-draw-omen-011/index.html"
+  },
+  {
+    "revision": "845b176b2af1263a79cf",
+    "url": "/react-simple-draw-omen-011/static/css/main.3634ebcc.chunk.css"
+  },
+  {
+    "revision": "5694e1898ca4c92fd14d",
+    "url": "/react-simple-draw-omen-011/static/js/2.6408f43b.chunk.js"
+  },
+  {
+    "revision": "845b176b2af1263a79cf",
+    "url": "/react-simple-draw-omen-011/static/js/main.a21d8b3a.chunk.js"
+  },
+  {
+    "revision": "bacdd99b024bd6da46b9",
+    "url": "/react-simple-draw-omen-011/static/js/runtime~main.e3ec137f.js"
+  }
+]);
